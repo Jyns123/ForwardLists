3c6a19998d1d737f7b7e4d283adbacd935843a6f
Jyns123 <ordonezjyns@gmail.com>
2024-09-05 12:55:42 -0500

    xd

--- doblyLinkedList/ec.cpp
+++ doblyLinkedList/ec.cpp
@@ -1,256 +1,4 @@
-#include <iostream>
-using namespace std;
-
-class Node {
-public:
-    int val;
-    Node* prev;
-    Node* next;
-    Node* child;
-    Node(): prev(nullptr), val(0), next(nullptr){}
-    Node(int _val): val(_val), prev(nullptr), next(nullptr){}
-    Node(Node* _prev ,int _val): val(_val), prev(_prev), next(nullptr){}
-    Node(int _val, Node* _next): val(_val), prev(nullptr), next(_next){}
-    Node(Node* _prev,int _val, Node* _next): val(_val), prev(_prev), next(_next){}
-};
-
-class ListNodeOperations {
-private:
-    Node* head;
-    Node* tail;
-
-public:
-    ListNodeOperations(): head(nullptr), tail(nullptr){};
-    int front();
-    int back();
-    void push_front(int val);
-    void push_back(int val);
-    void pop_front();
-    void pop_back();
-    void insert(int val, int index);
-    void remove(int index);
-    int& operator[](int index);
-    bool empty();
-    int size();
-    void clear();
-    void reverse();
-    ListNodeOperations& operator=(const ListNodeOperations& other);
-};
-
-int ListNodeOperations::front() {
-    if (head != nullptr) {
-        return head->val;
-    }
-    throw out_of_range("La lista está vacía");
-}
-
-int ListNodeOperations::back() {
-    if (tail != nullptr) {
-        return tail->val;
-    }
-    throw out_of_range("La lista está vacía");
-}
-
-void ListNodeOperations::push_front(int val){
-    if(!head){
-        head = tail = new Node(val);
-    } else {
-        Node* NewHead = new Node(val, head);
-        head->prev = NewHead;
-        head = NewHead;
-    }
-}
-
-void ListNodeOperations::push_back(int val){
-    if(!head){
-        head = tail = new Node(val);
-    } else {
-        Node* NewTail = new Node(tail, val);
-        tail->next = NewTail;
-        tail = NewTail;
-    }
-}
-
-void ListNodeOperations::pop_front(){
-    if(!head){
-        return;
-    } else {
-        Node* temp = head;
-        head = head->next;
-        if (head) {
-            head->prev = nullptr;
-        } else {
-            tail = nullptr;       
-        }
-        delete temp;
-    }
-}
-
-void ListNodeOperations::pop_back(){
-    if(!head){
-        return;
-    } else {
-        Node* temp = tail;
-        tail = tail->prev;
-        if (tail) {
-            tail->next = nullptr;
-        } else {
-            head = nullptr;       
-        }
-        delete temp;
-    }
-}
-
-void ListNodeOperations::insert(int val, int index) {
-    if (index < 0 || index > size()) { 
-        return;
-    }
-    if (index == 0) {
-        push_front(val);
-        return;
-    }
-    if (index == size()) {
-        push_back(val);
-        return;
-    }
-
-    Node* curr = head;
-    int count = 0;
-    while (count < index) {
-        curr = curr->next;
-        count++;
-    }
-
-    Node* NewNode = new Node(curr->prev, val, curr);
-    curr->prev->next = NewNode;
-    curr->prev = NewNode;
-}
-
-void ListNodeOperations::remove(int index) {
-    if (index < 0 || index >= size()) {
-        return;
-    }
-
-    if (index == 0) {
-        pop_front();
-        return;
-    }
-    if (index == size() - 1) {
-        pop_back();
-        return;
-    }
-
-    Node* curr = head;
-    int count = 0;
-    while (count < index) {
-        curr = curr->next;
-        count++;
-    }
-
-    curr->prev->next = curr->next;
-    curr->next->prev = curr->prev;
-    delete curr;
-}
-
-int& ListNodeOperations::operator[](int index) {
-    if (index < 0) {
-        throw out_of_range("Índice no válido");
-    }
-
-    Node* current = head;
-    int currentIndex = 0;
-
-    while (current != nullptr && currentIndex < index) {
-        current = current->next;
-        currentIndex++;
-    }
-
-    if (current == nullptr) {
-        throw out_of_range("Índice fuera de rango");
-    }
-
-    return current->val;
-}
-
-bool ListNodeOperations::empty() {
-    return head == nullptr;
-}
-
-int ListNodeOperations::size() {
-    int count{};
-    Node* curr = head;
-    while(curr) {
-        curr = curr->next;
-        count++; 
-    }
-    return count;
-}
-
-void ListNodeOperations::clear() {
-    while (!empty()) {
-        pop_front();
-    }
-    head = tail = nullptr;
-}
-
-void ListNodeOperations::reverse() {
-    if (!head) {
-        return;
-    }
-
-    Node* curr = head;
-    Node* temp = nullptr;
-
-    while (curr) {
-        temp = curr->prev;
-        curr->prev = curr->next;
-        curr->next = temp;
-
-        curr = curr->prev; 
-    }
-
-    temp = head;
-    head = tail;
-    tail = temp;
-}
-
-class Solution {
-public:
-    Node* flatten(Node* head) {
-        if (!head) return nullptr;
-        
-        Node* curr = head;
-        Node* tail = head;
-        
-        while (curr) {
-            if (curr->child) {
-                Node* child = curr->child;
-                Node* childTail = child;
-                
-                while (childTail->next) {
-                    childTail = childTail->next;
-                }
-
-                Node* nextcurNode = curr->next;
-                curr->next = child;
-                child->prev = curr;
-                curr->child = nullptr;
-                childTail->next = nextcurNode;
-
-                if (nextcurNode != nullptr) {
-                    nextcurNode->prev = childTail;
-                }
-            }
-            curr = curr->next;
-        }
-        
-        return head;
-    }
-};
-
-
-
-/*
+//LRU Cache Implementación (Lista Doble Simple)
 struct Node {
     int key;
     int value;
@@ -327,9 +75,9 @@ public:
             capacity--;
         }
     }
-};*/
+};
 
-/*
+//LRU Cache Implementación (Con unordered_map)
 #include <unordered_map>
 
 struct Node {
@@ -416,9 +164,7 @@ public:
     }
 };
 
-*/
-
-/*
+//
 struct Node {
     string url;
     Node* prev;
@@ -458,9 +204,9 @@ public:
         }
         return current->url;
     }
-};*/
+};
 
-/*
+//LFU Cache Implementación (Con unordered_map y list)
 #include <iostream>
 #include <unordered_map>
 #include <list>
@@ -560,10 +306,9 @@ public:
         freqListMap[1].addNode(newNode);
         size++;
     }
-};*/
+};
 
-
-/*
+//LFU Cache Implementación (Con DoublyLinkedList y Array de Punteros)
 #include <iostream>
 #include <limits.h>
 using namespace std;
@@ -700,4 +445,4 @@ public:
         }
         size++;
     }
-};*/
+};
