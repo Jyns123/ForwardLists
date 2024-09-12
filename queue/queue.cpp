807529a15ac167310d0585b2c90cdba2b3c7023b
Jyns123 <ordonezjyns@gmail.com>
2024-09-12 11:08:32 -0500

    queue

--- queue/queue.cpp
+++ queue/queue.cpp
@@ -3,6 +3,24 @@
 
 using namespace std;
 
+class RecentCounter {
+private:
+    queue<int> cola;  
+
+public:
+    RecentCounter() {
+    }
+    
+    int ping(int t) {
+        cola.push(t);
+        
+        while (!cola.empty() && cola.front() < t - 3000) {
+            cola.pop();
+        }
+        
+        return cola.size();
+    }
+};
 
 class MyQueue {
 private:
@@ -47,6 +65,60 @@ public:
         }
         
         return -1;
+    }
+
+    int countStudents(vector<int>& students, vector<int>& sandwiches) {
+        int count0{}, count1{};
+        for (auto it = students.begin(); it != students.end(); ++it) {
+            if (*it == 0) count0++;
+            else count1++;
+        }
+
+        for (auto it = sandwiches.begin(); it != sandwiches.end(); ++it) {
+            if (*it == 0 && count0 == 0) {
+                break;
+            }
+            if (*it == 1 && count1 == 0) {
+                break;  
+            }
+
+            if (*it == 0) {
+                count0--;  
+            } else {
+                count1--;  
+            }
+        }
+
+        return count0 + count1;
+    }
+
+    int timeRequiredToBuy(vector<int>& tickets, int k) {
+        queue<int> q;  
+        int n = tickets.size(); 
+        
+        for (int i = 0; i < n; i++) {
+            q.push(i);
+        }
+        
+        int time = 0;  
+        
+        while (!q.empty()) {
+            int idx = q.front();
+            q.pop();
+            
+            tickets[idx]--;
+            time++;  
+            
+            if (idx == k && tickets[idx] == 0) {
+                break;
+            }
+            
+            if (tickets[idx] > 0) {
+                q.push(idx);
+            }
+        }
+        
+        return time;
     }
 
 };
